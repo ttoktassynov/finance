4425b64f69c8a0acf5666778bac5a31e04b8ffe0
Timur <timur.toktassynov@gmail.com>
2021-03-12 13:46:41 +0000

    fixed the db to postgres

--- application.py
+++ application.py
@@ -1,5 +1,4 @@
 import os
-
 from cs50 import SQL
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
 from flask_session import Session
@@ -33,8 +32,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///finance.db")
-
+db = SQL(os.getenv("DATABASE_URL"))
 # Make sure API key is set
 if not os.environ.get("API_KEY"):
     raise RuntimeError("API_KEY not set")
